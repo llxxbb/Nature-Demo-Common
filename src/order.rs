af18ba7e3add56cb8436a44bfda9e85ffc7c561a
llxxbb <llxxbb@yeah.net>
2019-09-01 19:26:42 +0800

    create fetch method for Nature

--- src/order.rs
+++ src/order.rs
@@ -1,14 +1,19 @@
+#[derive(Serialize, Deserialize, Debug, Default, Clone, PartialEq, Eq)]
 pub struct Commodity {
     pub id: u32,
     pub name: String,
 }
 
+#[derive(Serialize, Deserialize, Debug, Default, Clone, PartialEq, Eq)]
 pub struct SelectedCommodity {
     pub item: Commodity,
     pub num: u32,
 }
 
+#[derive(Serialize, Deserialize, Debug, Default, Clone, PartialEq, Eq)]
 pub struct Order {
+    pub user_id: u32,
     pub price: u32,
     pub items: Vec<SelectedCommodity>,
+    pub address: String,
 }
